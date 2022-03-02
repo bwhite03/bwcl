9a4fca6e01f05151d2ee441118f3da07b9c33208
bwhite03 <bwhite1195@yahoo.com>
2022-03-02 11:41:23 -0500

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+export interface ButtonProps {
+  label: string;
+  style: React.CSSProperties;
+  onClick: () => React.MouseEvent<HTMLButtonElement>;
+}
+
+const Button = (props: ButtonProps) => {
+  const { label, style, onClick } = props;
+  return (
+    <button onClick={onClick} style={style}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
